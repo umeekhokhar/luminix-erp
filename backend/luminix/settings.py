b633a69e41e36dcc296a0f29b307e056a094394a
Umar <m.umar6us@gmail.com>
2026-07-01 00:54:00 +0500

    cors

--- backend/luminix/settings.py
+++ backend/luminix/settings.py
@@ -141,6 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://luminixerp.netlify.app/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
